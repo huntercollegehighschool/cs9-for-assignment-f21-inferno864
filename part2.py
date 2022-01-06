5b2344aca5ae1463f2fbba14ea2cd838c693bfe4
Colby O'Connor <94643942+inferno864@users.noreply.github.com>
2022-01-06 00:54:00 +0000

    Part 2

--- part2.py
+++ part2.py
@@ -8,3 +8,17 @@ For example, if the user types in 4, the program should print 100 (since 1^3 + 2
 """
 
 #write your code here
+number = int(input("Enter a number: "))
+cube = number
+number2 = number
+total = cube 
+cube2 = 0
+for i in range(number + 1, 1, -1): 
+  cube2 = cube
+  cube = cube2
+  cube = number * number * number
+  number -= 1 
+  total = cube + cube2
+  cube = total
+  
+print(total - number2)
